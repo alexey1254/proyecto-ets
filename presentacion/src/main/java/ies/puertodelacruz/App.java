7d15b31b8402e279d908f04ace62ccf180c9e4fe
Grace Stephanie Caraballo Pose <grace.caraballop@gmail.com>
2022-03-09 15:08:50 +0000

    Se crea el modulo presentacion

---
+++ presentacion/src/main/java/ies/puertodelacruz/App.java
@@ -0,0 +1,13 @@
+package ies.puertodelacruz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
