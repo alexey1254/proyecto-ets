e29cdcb0e811ec23d7c2cbac0ea189de91158ad4
alexey1254 <alxkill54@gmail.com>
2022-03-24 19:36:07 +0000

    Se termina de realizar las clases close #7;

--- logica/src/main/java/es/iespuertodelacruz/ets/Compra.java
+++ logica/src/main/java/es/iespuertodelacruz/ets/Compra.java
@@ -1,7 +1,100 @@
 package main.java.es.iespuertodelacruz.ets;
 
-
+/**
+ * @author Alejandro
+ * Clase compra
+ */
 
 public class Compra {
+    private int numFactura;
+    private Producto producto;
+    private double cantidad;
+    private Fecha fecha;
+
+    /**
+     * Constructor vacio
+     */
+    public Compra() {
+
+    }
+
+    /**
+     * Constructor con parametros
+     * @param numFactura de la compra
+     * @param producto que se compra
+     * @param cantidad que se compra
+     * @param fecha de la compra
+     */
+    public Compra(int numFactura, Producto producto, double cantidad, Fecha fecha) {
+        this.numFactura = numFactura;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.fecha = fecha;
+    }
+
+    /**
+     * Getter numero de factura
+     * @return numero de factura
+     */
+    public int getNumFactura() {
+        return numFactura;
+    }
+
+    /**
+     * Setter del numero de factura
+     * @param numFactura Numero de factura
+     */
+    public void setNumFactura(int numFactura) {
+        this.numFactura = numFactura;
+    }
+
+    /**
+     * Getter del producto
+     * @return producto que el cliente ha comprado
+     */
+    public Producto getProducto() {
+        return producto;
+    }
+
+    /**
+     * Setter del producto
+     * @param producto que el cliente compra
+     */
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    /**
+     * Getter de cantidad
+     * @return Cantidad del producto que compra
+     */
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * Setter de cantidad
+     * @param cantidad del producto que compra
+     */
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * Getter de la fecha de compra
+     * @return fecha de la compra
+     */
+    public Fecha getFecha() {
+        return fecha;
+    }
+
+    /**
+     * Setter de la fecha de compra
+     * @param fecha de la compra
+     */
+    public void setFecha(Fecha fecha) {
+        this.fecha = fecha;
+    }
+    
     
 }
