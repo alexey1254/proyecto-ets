c0f6ac99975a92fdb5267b0a7b9c67b634641ea7
alexey1254 <alxkill54@gmail.com>
2022-05-27 16:37:01 +0100

    Se eliminan los metodos set codigo, dni,apellido1, ap2, direccion de la clase cliente porque son heredados y no hace falta implementarlos porque se heredan de la clase persona y la implentacion de los setters son iguales

--- model/src/main/java/es/iespuertodelacruz/ets/Cliente.java
+++ model/src/main/java/es/iespuertodelacruz/ets/Cliente.java
@@ -2,26 +2,29 @@ package es.iespuertodelacruz.ets;
 
 /**
  * Clase Cliente.
+ * 
  * @author Grace
  */
-public class Cliente extends Persona{
+public class Cliente extends Persona {
     private int codigo;
     private String direccion;
-    
+
     /**
      * Constructor que recibe tres parmetros
-     * @param codigo del cliente
-     * @param datos dni, nombre y/o apellido del cliente
+     * 
+     * @param codigo    del cliente
+     * @param datos     dni, nombre y/o apellido del cliente
      * @param direccion del cliente
      */
-    public Cliente(int codigo,String dni, String nombre, String apellido1, String apellido2, String direccion) {
-        super(dni, nombre, apellido1,apellido2);
+    public Cliente(int codigo, String dni, String nombre, String apellido1, String apellido2, String direccion) {
+        super(dni, nombre, apellido1, apellido2);
         this.codigo = codigo;
         this.direccion = direccion;
     }
 
     /**
      * Getter del codigo del cliente
+     * 
      * @return codigo del cliente
      */
     public int getCodigo() {
@@ -29,39 +32,8 @@ public class Cliente extends Persona{
     }
 
     /**
-     * Setter del dni del cliente
-     * @param dni Nuevo dni del cliente
-     */
-    public void setDni(String dni) {
-        super.setDni(dni);
-    }
-
-    /**
-     * Setter del nombre del cliente
-     * @param nombre Nuevo nombre del cliente
-     */
-    public void setNombre(String nombre) {
-        super.setNombre(nombre);
-    }
-
-    /**
-     * Setter del primer apellido del cliente
-     * @param apellido1 Nuevo primer apellido del cliente
-     */
-    public void setApellido1(String apellido1) {
-        super.setApellido1(apellido1);
-    }
-
-    /**
-     * Setter del segundo apellido del cliente
-     * @param apellido2 Nuevo segundo apellido del cliente
-     */
-    public void setApellido2(String apellido2) {
-        super.setApellido2(apellido2);
-    }
-
-    /**
      * Setter del cargo del cliente
+     * 
      * @param direccion Nueva direccion del cliente
      */
     public void setDireccion(String direccion) {
